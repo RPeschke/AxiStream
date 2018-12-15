156c5076db0f47e3c8bb4d9f296df0a0e4988f7d
rpeschke <peschke@hawaii.edu>
2018-12-15 00:34:59 -1000

    added the make project script to the build system

--- make_buildsystem.sh
+++ make_buildsystem.sh
@@ -5,5 +5,5 @@ mkdir build/coregen/ipcore
 
 python protoBuild/makeise/makeise.py  protoBuild/AxiStreamTest_Project.in build/axiTest.xise
 
-
+python protoBuild/makeise/makePrj.py protoBuild/AxiStreamTest_Project.in build/
 python protoBuild/pyMakeScript.py
